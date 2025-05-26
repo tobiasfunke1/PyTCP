2ec32eb4316681089c6459ab39dcc720e7cf1787
Tobias Funke <Tobias Funke 52739350+tobiasfunke1@users.noreply.github.com>
2025-05-26 17:11:58 +0200

    remove _tx_buffer storage

--- pytcp/protocols/tcp/session.py
+++ pytcp/protocols/tcp/session.py
@@ -413,7 +413,7 @@ class TcpSession:
         """
         if self._state in {FsmState.ESTABLISHED, FsmState.CLOSE_WAIT}:
             with self._lock_tx_buffer:
-                self._tx_buffer.extend(data)
+                # self._tx_buffer.extend(data)
                 return len(data)
         # This error should be risen when session is locally or fully closed
         raise TcpSessionError(
@@ -762,7 +762,8 @@ class TcpSession:
             )
         # Purge acked data from TX buffer
         with self._lock_tx_buffer:
-            del self._tx_buffer[: self._tx_buffer_una]
+            # del self._tx_buffer[: self._tx_buffer_una]
+            pass
         self._tx_buffer_seq_mod += self._tx_buffer_una
         __debug__ and log(
             "tcp-ss",
